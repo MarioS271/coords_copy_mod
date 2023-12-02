37b4f75c900e0ff397609afc12992bbb4ab1259d
MarioS271 <marioerik.scholze@gmail.com>
2023-12-02 23:15:07 +0100

    Initial Commit

---
+++ src/main/java/net/marios271/coords_copy/CoordsCopyClient.java
@@ -0,0 +1,10 @@
+package net.marios271.coords_copy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CoordsCopyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
