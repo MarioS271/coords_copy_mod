ff757178d93935e3b460da07a757bceae4466896
MarioS271 <marioerik.scholze@gmail.com>
2024-06-23 20:41:56 +0200

    Minor changes

--- src/main/java/net/marios271/coords_copy/CoordsCopy.java
+++ src/main/java/net/marios271/coords_copy/CoordsCopy.java
@@ -1,13 +1,16 @@
 package net.marios271.coords_copy;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@Environment(EnvType.CLIENT)
 public class CoordsCopy implements ModInitializer {
 	public static final String MOD_ID = "coords_copy";
-    public static final Logger LOGGER = LoggerFactory.getLogger("coords_copy");
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
--- src/main/java/net/marios271/coords_copy/CoordsCopyClient.java
+++ src/main/java/net/marios271/coords_copy/CoordsCopyClient.java
@@ -1,8 +1,11 @@
 package net.marios271.coords_copy;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.marios271.coords_copy.event.KeyInputHandler;
 
+@Environment(EnvType.CLIENT)
 public class CoordsCopyClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
